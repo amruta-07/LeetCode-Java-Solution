87121a9cb65e7eb074124530fa3c1ce0044dad4b
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-11-10 23:10:32 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        // System.out.println("Hello");
+        int a;
+        System.out.println("Enter Number");
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        System.out.println(a);
+    }
+
+}
